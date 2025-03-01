e8ab621b3bbbf039cf36f6140027f7d538df8f30
Léo Gaspard <leo@gaspard.io>
2025-03-01 02:37:16 +0100

    introduce the two first users

--- shell.nix
+++ shell.nix
@@ -1,28 +1,20 @@
 let
   pkgs = import ./nix;
 in
-pkgs.stdenv.mkDerivation {
-  name = "indexed-db-rs";
-  buildInputs = (
-    (with pkgs; [
-      cargo-bolero
-      cargo-nextest
-      #chromedriver
-      #chromium
-      firefox
-      geckodriver
-      just
-      niv
-      wasm-bindgen-cli_0_2_100
-      wasm-pack
+pkgs.mkShell.override {
+  stdenv = pkgs.clangStdenv;
+} {
+  name = "sakuhiki";
+  buildInputs = with pkgs; [
+    niv
+    pkg-config
 
-      (fenix.combine (with fenix; [
-        minimal.cargo
-        minimal.rustc
-        complete.rust-src
-        rust-analyzer
-        targets.wasm32-unknown-unknown.latest.rust-std
-      ]))
-    ])
-  );
+    (fenix.combine (with fenix; [
+      minimal.cargo
+      minimal.rustc
+      complete.rust-src
+      rust-analyzer
+      targets.wasm32-unknown-unknown.latest.rust-std
+    ]))
+  ];
 }
