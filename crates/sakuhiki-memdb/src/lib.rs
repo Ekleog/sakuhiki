1f77d6f706149728b6702799ff1fc0c9eb57fe5f
Léo Gaspard <leo@gaspard.io>
2025-03-01 22:42:42 +0100

    cleanup memdb code

--- crates/sakuhiki-memdb/src/lib.rs
+++ crates/sakuhiki-memdb/src/lib.rs
@@ -1,7 +1,7 @@
 use std::{
+    borrow::{Borrow, BorrowMut},
     collections::BTreeMap,
-    future::Future,
-    future::{ready, Ready},
+    future::{ready, Future, Ready},
     ops::RangeBounds,
     pin::Pin,
 };
@@ -17,7 +17,7 @@ pub struct MemDb {
 impl sakuhiki::Backend for MemDb {
     type Error = std::convert::Infallible;
 
-    type RoTransaction<'t> = RoTransaction<'t>;
+    type RoTransaction<'t> = Transaction<&'t BTreeMap<Vec<u8>, Vec<u8>>>;
 
     // TODO(blocked): return impl Future (and in all the other Pin<Box<dyn Future<...>> too)
     type RoTransactionFuture<'t, F, Return>
@@ -35,12 +35,12 @@ impl sakuhiki::Backend for MemDb {
     {
         Box::pin(async {
             let db = self.db.read().await;
-            let t = RoTransaction { db: &db };
+            let t = Transaction { db: &*db };
             Ok(actions(&t).await)
         })
     }
 
-    type RwTransaction<'t> = RwTransaction<'t>;
+    type RwTransaction<'t> = Transaction<&'t mut BTreeMap<Vec<u8>, Vec<u8>>>;
 
     type RwTransactionFuture<'t, F, Return>
         = Pin<Box<dyn 't + Future<Output = Result<Return, Self::Error>>>>
@@ -57,88 +57,79 @@ impl sakuhiki::Backend for MemDb {
     {
         Box::pin(async {
             let mut db = self.db.write().await;
-            let t = RwTransaction { db: &mut db };
+            let t = Transaction { db: &mut *db };
             Ok(actions(&t).await)
         })
     }
 }
 
-pub struct RoTransaction<'t> {
-    db: &'t BTreeMap<Vec<u8>, Vec<u8>>,
+pub struct Transaction<Ref> {
+    db: Ref,
 }
 
-macro_rules! impl_ro_transaction {
-    ($struct:ident) => {
-        impl<'t> sakuhiki::backend::RoTransaction for $struct<'t> {
-            type Error = std::convert::Infallible;
+impl<Ref> sakuhiki::backend::RoTransaction for Transaction<Ref>
+where
+    Ref: Borrow<BTreeMap<Vec<u8>, Vec<u8>>>,
+{
+    type Error = std::convert::Infallible;
 
-            type Key<'db>
-                = &'db [u8]
-            where
-                Self: 'db;
+    type Key<'db>
+        = &'db [u8]
+    where
+        Self: 'db;
 
-            type Value<'db>
-                = &'db [u8]
-            where
-                Self: 'db;
+    type Value<'db>
+        = &'db [u8]
+    where
+        Self: 'db;
 
-            type GetFuture<'key, 'db>
-                = Ready<Result<Option<Self::Key<'db>>, Self::Error>>
-            where
-                Self: 'db,
-                'db: 'key;
+    type GetFuture<'key, 'db>
+        = Ready<Result<Option<Self::Key<'db>>, Self::Error>>
+    where
+        Self: 'db,
+        'db: 'key;
 
-            fn get<'db, 'key>(&'db mut self, key: &'key [u8]) -> Self::GetFuture<'key, 'db>
-            where
-                'db: 'key,
-            {
-                ready(Ok(self.db.get(key).map(|v| v.as_slice())))
-            }
+    fn get<'db, 'key>(&'db mut self, key: &'key [u8]) -> Self::GetFuture<'key, 'db>
+    where
+        'db: 'key,
+    {
+        ready(Ok(self.db.borrow().get(key).map(|v| v.as_slice())))
+    }
 
-            type ScanStream<'keys, 'db>
-                = Pin<
-                Box<
-                    dyn 'keys
-                        + Stream<Item = Result<(Self::Key<'db>, Self::Value<'db>), Self::Error>>,
-                >,
-            >
-            where
-                Self: 'db,
-                'db: 'keys;
+    type ScanStream<'keys, 'db>
+        =
+        Pin<Box<dyn 'keys + Stream<Item = Result<(Self::Key<'db>, Self::Value<'db>), Self::Error>>>>
+    where
+        Self: 'db,
+        'db: 'keys;
 
-            fn scan<'db, 'keys>(
-                &'db mut self,
-                keys: impl 'keys + RangeBounds<[u8]>,
-            ) -> Self::ScanStream<'keys, 'db>
-            where
-                'db: 'keys,
-            {
-                Box::pin(stream::iter(
-                    self.db
-                        .range(keys)
-                        .map(|(k, v)| Ok((k.as_slice(), v.as_slice()))),
-                ))
-            }
-        }
-    };
+    fn scan<'db, 'keys>(
+        &'db mut self,
+        keys: impl 'keys + RangeBounds<[u8]>,
+    ) -> Self::ScanStream<'keys, 'db>
+    where
+        'db: 'keys,
+    {
+        Box::pin(stream::iter(
+            self.db
+                .borrow()
+                .range(keys)
+                .map(|(k, v)| Ok((k.as_slice(), v.as_slice()))),
+        ))
+    }
 }
 
-impl_ro_transaction!(RoTransaction);
-
-pub struct RwTransaction<'t> {
-    db: &'t mut BTreeMap<Vec<u8>, Vec<u8>>,
-}
-
-impl_ro_transaction!(RwTransaction);
-
-impl sakuhiki::backend::RwTransaction for RwTransaction<'_> {
+impl<Ref> sakuhiki::backend::RwTransaction for Transaction<Ref>
+where
+    Ref: BorrowMut<BTreeMap<Vec<u8>, Vec<u8>>> + Borrow<BTreeMap<Vec<u8>, Vec<u8>>>,
+{
     type PutFuture<'db>
         = Ready<Result<(), Self::Error>>
     where
         Self: 'db;
 
     fn put<'db>(&'db mut self, key: &'db [u8], value: &'db [u8]) -> Self::PutFuture<'db> {
-        self.db.insert(key.to_vec(), value.to_vec());
+        self.db.borrow_mut().insert(key.to_vec(), value.to_vec());
         ready(Ok(()))
     }
 
@@ -148,7 +139,7 @@ impl sakuhiki::backend::RwTransaction for RwTransaction<'_> {
         Self: 'db;
 
     fn delete<'db>(&'db mut self, key: &'db [u8]) -> Self::DeleteFuture<'db> {
-        self.db.remove(key);
+        self.db.borrow_mut().remove(key);
         ready(Ok(()))
     }
 }
