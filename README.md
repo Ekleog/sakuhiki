f2e4c49b0d25921e8087e71d0e9063f33880fb6a
Léo Gaspard <leo@gaspard.io>
2025-03-01 02:19:25 +0100

    initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# Sakuhiki
+
+TODO
---
+++ shell.nix
@@ -0,0 +1,28 @@
+let
+  pkgs = import ./nix;
+in
+pkgs.stdenv.mkDerivation {
+  name = "indexed-db-rs";
+  buildInputs = (
+    (with pkgs; [
+      cargo-bolero
+      cargo-nextest
+      #chromedriver
+      #chromium
+      firefox
+      geckodriver
+      just
+      niv
+      wasm-bindgen-cli_0_2_100
+      wasm-pack
+
+      (fenix.combine (with fenix; [
+        minimal.cargo
+        minimal.rustc
+        complete.rust-src
+        rust-analyzer
+        targets.wasm32-unknown-unknown.latest.rust-std
+      ]))
+    ])
+  );
+}
