f9ced98434eebd18e362d2ff9ae32f4d6a730220
Léo Gaspard <leo@gaspard.io>
2025-03-02 05:12:49 +0100

    reexport sub-crates from sakuhiki

--- crates/sakuhiki/src/lib.rs
+++ crates/sakuhiki/src/lib.rs
@@ -1,14 +1,10 @@
-pub fn add(left: u64, right: u64) -> u64 {
-    left + right
-}
+pub use sakuhiki_core::*;
 
-#[cfg(test)]
-mod tests {
-    use super::*;
+#[cfg(feature = "db-indexed-db")]
+pub use sakuhiki_indexed_db::*;
 
-    #[test]
-    fn it_works() {
-        let result = add(2, 2);
-        assert_eq!(result, 4);
-    }
-}
+#[cfg(feature = "db-memdb")]
+pub use sakuhiki_memdb::*;
+
+#[cfg(feature = "db-rocksdb")]
+pub use sakuhiki_rocksdb::*;
