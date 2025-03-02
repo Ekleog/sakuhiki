8aebb39a04bc22f79c25b93d97c599d3ab662a19
Léo Gaspard <leo@gaspard.io>
2025-03-02 05:13:17 +0100

    actually do not reexport all databases, that's going to be version-bumping too often

--- crates/sakuhiki/src/lib.rs
+++ crates/sakuhiki/src/lib.rs
@@ -1,10 +1,1 @@
 pub use sakuhiki_core::*;
-
-#[cfg(feature = "db-indexed-db")]
-pub use sakuhiki_indexed_db::*;
-
-#[cfg(feature = "db-memdb")]
-pub use sakuhiki_memdb::*;
-
-#[cfg(feature = "db-rocksdb")]
-pub use sakuhiki_rocksdb::*;
