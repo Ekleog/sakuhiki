4bd3a55c2a8f6bcb6bad94b585b7893298dcf793
Léo Gaspard <leo@gaspard.io>
2025-03-02 22:26:39 +0100

    implement btree::index

--- crates/sakuhiki-core/src/index.rs
+++ crates/sakuhiki-core/src/index.rs
@@ -1,43 +1,54 @@
+use std::pin::Pin;
+
 use crate::{Backend, Datum, IndexError};
+
+type DynFuture<'fut, T> = Pin<Box<dyn 'fut + Future<Output = T>>>;
 
 pub trait Index<B: Backend> {
     type Datum: Datum<B>;
 
     fn cf(&self) -> &'static str;
 
-    fn index<'t>(
-        &self,
-        datum: &Self::Datum,
-        transaction: &mut B::RwTransaction<'t>,
-        cf: &mut B::RwTransactionCf<'t>,
-    ) -> Result<(), B::Error>;
+    fn index<'fut, 't>(
+        &'fut self,
+        key: &'fut [u8],
+        datum: &'fut Self::Datum,
+        transaction: &'fut mut B::RwTransaction<'t>,
+        cf: &'fut mut B::RwTransactionCf<'t>,
+    ) -> DynFuture<'fut, Result<(), B::Error>>;
 
     fn unindex<'t>(
         &self,
+        key: &[u8],
         datum: &Self::Datum,
         transaction: &mut B::RwTransaction<'t>,
         cf: &mut B::RwTransactionCf<'t>,
     ) -> Result<(), B::Error>;
 
-    fn index_from_slice<'t>(
-        &self,
-        slice: &[u8],
-        transaction: &mut B::RwTransaction<'t>,
-        cf: &mut B::RwTransactionCf<'t>,
-    ) -> Result<(), IndexError<B, Self::Datum>> {
-        let datum = Self::Datum::from_slice(slice).map_err(IndexError::Parsing)?;
-        self.index(&datum, transaction, cf)
-            .map_err(IndexError::Backend)
+    fn index_from_slice<'fut, 't>(
+        &'fut self,
+        key: &'fut [u8],
+        slice: &'fut [u8],
+        transaction: &'fut mut B::RwTransaction<'t>,
+        cf: &'fut mut B::RwTransactionCf<'t>,
+    ) -> DynFuture<'fut, Result<(), IndexError<B, Self::Datum>>> {
+        Box::pin(async move {
+            let datum = Self::Datum::from_slice(slice).map_err(IndexError::Parsing)?;
+            self.index(key, &datum, transaction, cf)
+                .await
+                .map_err(IndexError::Backend)
+        })
     }
 
     fn unindex_from_slice<'t>(
         &self,
+        key: &[u8],
         slice: &[u8],
         transaction: &mut B::RwTransaction<'t>,
         cf: &mut B::RwTransactionCf<'t>,
     ) -> Result<(), IndexError<B, Self::Datum>> {
         let datum = Self::Datum::from_slice(slice).map_err(IndexError::Parsing)?;
-        self.unindex(&datum, transaction, cf)
+        self.unindex(key, &datum, transaction, cf)
             .map_err(IndexError::Backend)
     }
 }
--- crates/sakuhiki-index-btree/src/lib.rs
+++ crates/sakuhiki-index-btree/src/lib.rs
@@ -1,14 +1,69 @@
-pub fn add(left: u64, right: u64) -> u64 {
-    left + right
+use std::{marker::PhantomData, pin::Pin};
+
+use sakuhiki_core::{Backend, Datum, Index, backend::RwTransaction};
+
+// TODO: add sub-index, instead of forcing the object key
+pub struct BTreeIndex<D, KeyExtractor> {
+    cf: &'static str,
+    key_extractor: KeyExtractor,
+    delimiter: Option<u8>,
+    _phantom: PhantomData<D>,
 }
 
-#[cfg(test)]
-mod tests {
-    use super::*;
+impl<B, D, KeyExtractor, K> Index<B> for BTreeIndex<D, KeyExtractor>
+where
+    B: Backend,
+    D: Datum<B>,
+    KeyExtractor: Fn(&D) -> K,
+    K: AsRef<[u8]>,
+{
+    type Datum = D;
 
-    #[test]
-    fn it_works() {
-        let result = add(2, 2);
-        assert_eq!(result, 4);
+    fn cf(&self) -> &'static str {
+        self.cf
+    }
+
+    fn index<'fut, 't>(
+        &'fut self,
+        key: &'fut [u8],
+        datum: &'fut Self::Datum,
+        transaction: &'fut mut B::RwTransaction<'t>,
+        cf: &'fut mut B::RwTransactionCf<'t>,
+    ) -> Pin<Box<dyn 'fut + Future<Output = Result<(), B::Error>>>> {
+        Box::pin(async move {
+            let index = (self.key_extractor)(datum);
+            let index = index.as_ref();
+            match self.delimiter {
+                None => {
+                    let mut index_key = Vec::with_capacity(index.len() + key.len());
+                    index_key.extend(index);
+                    index_key.extend(key);
+                    transaction.put(cf, &index_key, key).await?;
+                }
+                Some(delimiter) => {
+                    let mut index_key = Vec::with_capacity(index.len() * 2 + 1 + key.len());
+                    for c in index {
+                        index_key.push(*c);
+                        if *c == delimiter {
+                            index_key.push(delimiter);
+                        }
+                    }
+                    index_key.push(delimiter);
+                    index_key.extend(key);
+                    transaction.put(cf, &index_key, key).await?;
+                }
+            }
+            Ok(())
+        })
+    }
+
+    fn unindex<'t>(
+        &self,
+        _key: &[u8],
+        _datum: &Self::Datum,
+        _transaction: &mut B::RwTransaction<'t>,
+        _cf: &mut B::RwTransactionCf<'t>,
+    ) -> Result<(), B::Error> {
+        todo!()
     }
 }
