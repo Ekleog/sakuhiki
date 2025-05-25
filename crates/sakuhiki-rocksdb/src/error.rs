4236b1e68972664e30f69f1446e4be32bacac47b
Léo Gaspard <leo@gaspard.io>
2025-05-26 01:20:36 +0200

    remove no-longer-needed error variant

--- crates/sakuhiki-rocksdb/src/error.rs
+++ crates/sakuhiki-rocksdb/src/error.rs
@@ -6,34 +6,15 @@ pub type Result<T> = StdResult<T, Error>;
 #[error("{operation}")]
 pub struct Error {
     operation: ErrorWhile,
-    source: ErrorSource,
+    source: rocksdb::Error,
 }
 
 impl Error {
     pub(crate) fn rocksdb(operation: ErrorWhile, source: rocksdb::Error) -> Self {
-        Self {
-            operation,
-            source: ErrorSource::RocksDb(source),
-        }
-    }
-
-    pub(crate) fn spawn_blocking(operation: ErrorWhile, source: tokio::task::JoinError) -> Self {
-        Self {
-            operation,
-            source: ErrorSource::SpawnBlocking(source),
-        }
+        Self { operation, source }
     }
 }
 
 #[derive(Debug, derive_more::Display)]
 #[non_exhaustive]
 pub enum ErrorWhile {}
-
-#[derive(Debug, thiserror::Error)]
-enum ErrorSource {
-    #[error("RocksDb call failed")]
-    RocksDb(#[source] rocksdb::Error),
-
-    #[error("Failed joining blocking task")]
-    SpawnBlocking(#[source] tokio::task::JoinError),
-}
